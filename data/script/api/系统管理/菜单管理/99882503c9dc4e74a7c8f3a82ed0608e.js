ffc39f800b4341386487bb0f887f453e2af65909
郭亮 <2292735470@qq.com>
2022-10-24 23:04:55 +0800

    feat: 优化代码

---
+++ data/script/api/系统管理/菜单管理/99882503c9dc4e74a7c8f3a82ed0608e.js
@@ -0,0 +1,153 @@
+/*================================
+{
+  "type": "api",
+  "isFolder": false,
+  "method": "DELETE",
+  "fileName": "删除",
+  "path": "/delete",
+  "description": "",
+  "groupId": "1ba5cb19cc9c45f18bd64ef7b04fe876",
+  "fileLock": false,
+  "enabled": true,
+  "definition": {
+    "parameters": [
+      {
+        "description": "",
+        "expression": "",
+        "key": "id",
+        "dataType": "String",
+        "required": true,
+        "error": "",
+        "validateType": 0,
+        "value": "123456"
+      }
+    ],
+    "options": [],
+    "paths": [],
+    "headers": [],
+    "responseBodyDefinition": {
+      "responseType": "json",
+      "headers": [
+        {
+          "key": "connection",
+          "value": "keep-alive",
+          "description": ""
+        },
+        {
+          "key": "content-length",
+          "value": "69",
+          "description": ""
+        },
+        {
+          "key": "content-type",
+          "value": "application/json; charset=utf-8",
+          "description": ""
+        },
+        {
+          "key": "date",
+          "value": "Mon, 24 Oct 2022 14:27:31 GMT",
+          "description": ""
+        },
+        {
+          "key": "keep-alive",
+          "value": "timeout=5",
+          "description": ""
+        },
+        {
+          "key": "vary",
+          "value": "Origin",
+          "description": ""
+        },
+        {
+          "key": "x-frame-options",
+          "value": "SAMEORIGIN",
+          "description": ""
+        },
+        {
+          "key": "x-xss-protection",
+          "value": "1; mode=block",
+          "description": ""
+        }
+      ],
+      "children": [
+        {
+          "children": [],
+          "dataType": "String",
+          "description": "",
+          "error": "",
+          "expression": "",
+          "key": "code",
+          "required": false,
+          "validateType": 0,
+          "value": "200"
+        },
+        {
+          "children": [],
+          "dataType": "Boolean",
+          "description": "",
+          "error": "",
+          "expression": "",
+          "key": "success",
+          "required": false,
+          "validateType": 0,
+          "value": "true"
+        },
+        {
+          "children": [],
+          "dataType": "String",
+          "description": "",
+          "error": "",
+          "expression": "",
+          "key": "message",
+          "required": false,
+          "validateType": 0,
+          "value": "OK"
+        },
+        {
+          "children": [],
+          "dataType": "Number",
+          "description": "",
+          "error": "",
+          "expression": "",
+          "key": "data",
+          "required": false,
+          "validateType": 0,
+          "value": "0"
+        },
+        {
+          "children": [],
+          "dataType": "Number",
+          "description": "",
+          "error": "",
+          "expression": "",
+          "key": "executeTime",
+          "required": false,
+          "validateType": 0,
+          "value": "9"
+        }
+      ],
+      "dataType": "Object",
+      "description": "",
+      "error": "",
+      "expression": "",
+      "json": "{\n  \"code\": \"200\",\n  \"success\": true,\n  \"message\": \"OK\",\n  \"data\": 0,\n  \"executeTime\": 9\n}",
+      "key": "",
+      "required": false,
+      "validateType": 0,
+      "value": ""
+    }
+  },
+  "returnType": "",
+  "updatedAt": "2022-10-24 22:40:56",
+  "createdAt": "2022-10-24 22:23:19",
+  "createdBy": "",
+  "updatedBy": "",
+  "id": "99882503c9dc4e74a7c8f3a82ed0608e"
+}
+================================*/
+const menuCacheDelete = await importFunction('/system/menu/cache/delete');
+
+await menuCacheDelete(query.id);
+
+// db.table('sys_role_menu').where().eq("menu_id", id).delete();
+return await db.table("sys_menu").logic().where().eq("id", query.id).delete();
